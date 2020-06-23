59e84c1d53fd49890d664bb41d09783df092cedf
apple <you@example.com>
2020-06-23 13:54:54 +0800

    cm

--- src/Resource/PayTokenRequest.php
+++ src/Resource/PayTokenRequest.php
@@ -67,6 +67,13 @@ class PayTokenRequest extends RequestAbstract
     protected $redirect_url;
 
     /**
+     * When payment success, HitPoints will redirect user to the specified url address
+     * It's optional and max length is 255
+     * @var string
+     */
+    protected $return_url;
+
+    /**
      * @param string $reference_id
      */
     public function setReferenceId($reference_id)
@@ -120,6 +127,13 @@ class PayTokenRequest extends RequestAbstract
     public function setRedirectUrl($redirect_url)
     {
         $this->redirect_url = $redirect_url;
+    }
+
+    /**
+     * @param $return_url
+     */
+    public function setReturnUrl($return_url){
+        $this->return_url = $return_url;
     }
 
 }
